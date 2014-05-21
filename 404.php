d7dd1eeb134d05148d1a065f60caccf29dfd5318
Justin <me@justinmaslin.com>
2014-05-21 14:58:30 -0400

    404 page to the rescue

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php 
+include_once('header.php');
+?>
+
+<div class="container-fluid">
+	<div class="row col-md-6 col-md-offset-3 text-center">
+		<p class="lead">Lets make a good 404 page <br><br>...eventually.</p>
+	</div>
+</div>
+
+
+<?php
+include_once('footer.php');
+?>
