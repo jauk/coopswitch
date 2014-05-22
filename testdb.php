c77e5a1c03292bcbc2eb1ad69932a7c562c5abbc
Justin <me@justinmaslin.com>
2014-05-22 16:04:28 -0400

    records are still off, will fix

--- testdb.php
+++ testdb.php
@@ -3,18 +3,18 @@ include('header.php');
 
 include_once('connect.php');
 
-$limit = 100;
+$limit = 101;
 $name = "";
 $password = "";
 $email = "";
 
 
-for ($x = 0; $x < $limit; $x++) {
+for ($x = 1; $x < $limit; $x++) {
 
 	$cycle = rand(1,2);
 	$num_year_program = rand(1,2);
 	$majorVal = rand(2,87);
-	$email = "$x@drexel.edu";
+	$email = "$x@drexel.edu"; 
 
 	$sql="INSERT INTO Users (name, password, email, cycle, num_year_program, major, register_date)
 		  VALUES ('$name','$password', '$email','$cycle', '$num_year_program', '$majorVal',
