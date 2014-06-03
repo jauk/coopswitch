40d413956e1c3659203f71a13ba91c8b26570ffc
Justin <me@justinmaslin.com>
2014-06-03 16:38:14 -0400

    New location

---
+++ stats/stats.php
@@ -0,0 +1,19 @@
+<?php include('header.php'); ?>
+
+<div class="container-fluid">
+
+
+    <?php include_once('stats_top.php'); ?>
+
+   
+   <!-- Stats for users who receive matches as dropped_matches increase somehow. -->
+
+
+<!-- Major specific stats for everything 
+     Get as specific as possible, enter major to get stats for, etc.
+     Make line graphs that go over time, etc. (ADD MATCHED DATE COLUMN TO Matches TABLE FOR THIS TOO)
+-->
+
+</div>
+
+<?php include('footer.php'); ?>
