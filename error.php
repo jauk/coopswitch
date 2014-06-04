2311ffd70af911bbb16b4e57eebe32b58b59bb00
Justin <me@justinmaslin.com>
2014-06-04 16:53:04 -0400

    Updated error page

--- error.php
+++ error.php
@@ -1,13 +1,35 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/config.php");
 require_once(TEMPLATES_PATH . "/header.php");
+
+if (isset($_GET['msg']))
+  $msg = test_input($_GET['msg']);
+
 ?>
 
 <div class="container-fluid">
 	<div class="row col-md-6 col-md-offset-3 text-center">
 
-		<p class="lead">There was an error! <br><br> Please try again. </p>
+		<?php 
 
+		if (isset($msg)) {
+			if ($msg == 1) { 
+				$error="Invalid email.";
+			}
+			else {
+				$error = "Unknown error!";
+			}
+		}
+		else { 
+			$error = "Generic error!";
+		}
+
+		?>
+
+		<p class="lead"> <?php echo "$error"; ?> </p>
+			<br> 
+		<p>Please try again.</p>
+	
 	</div>
 </div>
 
