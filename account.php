a868a7d4e7becc9e6e05a013447c44da0667671b
Justin <me@justinmaslin.com>
2014-07-02 09:14:28 -0400

    Revamp of major css for alignment

--- account.php
+++ account.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/config.php");
 require_once(TEMPLATES_PATH . "/header.php");
 include(FUNCTION_PATH . "/connect.php");
@@ -10,149 +10,177 @@ if ($_SESSION['login'] == "") {
 else if ($_SESSION['login'] == "1") {
 }
 
+$rowClass = "col-md-6 col-md-offset-3 text-center";
+$userFieldClass = "col-sm-2 col-sm-offset-4 text-center";
+
+$majorBeginClass = "";
+$majorBeginEditClass = "";
+
 ?>
 
 <!-- <hr> -->
 
 <div class="container-fluid">
 
-	<div class="row-fluid col-md-6 col-md-offset-3 text-center">
-		<div class="panel-heading">
-			<h2>Hello, <?php echo "{$_SESSION['user_name']}" ?>!</h2>
-		</div>
-	</div>
+  <div class="row">
+  	<div class="<?php echo $rowClass; ?>">
+  		<div class="panel-heading">
+  			<h2>Hello, <?php echo "{$_SESSION['user_name']}" ?>!</h2>
+  		</div>
+  	</div>
+  </div>
 
-	<div class="row-fluid col-sm-6 col-sm-offset-3 text-center">
-		<span class="error"><div id="droppedMatches"></div></span>
-	</div>
+  <div class="row">
+  	<div class="<?php echo $rowClass; ?>">
+  		<span class="error"><div id="droppedMatches"></div></span>
+  	</div>
+  </div>
 
 	<!-- Alight text left and edit buttons right eventually. -->
+	
 	<!-- Should I add confirmations? -->
 
 	<!-- Code for user's major -->
-	<div class="row-fluid col-sm-6 col-sm-offset-3 text-center">
-		<h4>
-			Your major is
-			<span>
-			<div style="display: inline-block;" id="majorName"><?php echo "{$_SESSION['user_major_name']}."; ?></div>
-			<a id="majorEditBtn" href="#" class="btn btn-xs btn-warning" onclick="editMajor()">Edit</a>
-			</span>
-		</h4>
-	</div>
-	<div class="row col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5 text-center">
-			<span id="majorSpan" style="display: none;">
-				<form id="majorForm" name="majorForm" method="post" action="update.php" onsubmit="return saveMajor();">
-					<input type="hidden" name="newMajorId" id="newMajorId" value="">
-					<select id="selectMajor" class="form-control selectpicker" name="major" data-live-search="true" data-size="5">
-						<?php print_majors(); ?>
-					</select>
-					<div style="padding-top: 5px;"> <!-- Tmp inline style -->
-					<button type="submit" name="majorSaveBtn" value="Submit" id="majorSaveBtn" class="btn btn-sm btn-success" style="display: none;">Save</button>
-					<a id="majorCancelBtn" href="#" class="btn btn-sm btn-info" onclick="cancelEdit('major')" style="display: none;">Cancel</a>
-					</div>
-				</form>
-			</span>
+	<div class="row">
+	
+	<!-- Need to change class widths for when edit mode enabled. Set names via php and use js to switch.
+	      Just for majorBeginText?
+	 -->
+	
+	<!-- Name major and text -->
+  	<div id="majorBeginText" class="col-sm-2 col-sm-offset-3" style="padding-right: 0px; text-align: right;">
+  		<h4>Your major is</h4>
+  	</div>
+  	
+  	<div id="majorNameText" class="col-sm-2" style="text-align: left; padding-left: 5px;">
+  	  <h4>
+  	   	<div style="display: inline-block;"><?php echo "{$_SESSION['user_major_name']}."; ?></div>
+  		</h4>
+  	</div>
+
+	<!-- User Major Field Editable -->
+    <form id="majorForm" name="majorForm" method="post" action="update.php" onsubmit="return saveMajor();">
+      <!-- Dropdown of majors -->
+    	<div id="majorSpan" style="display: none; padding-left: 5px;" class="col-sm-3">
+  				<input type="hidden" name="newMajorId" id="newMajorId" value="">
+  				<select id="selectMajor" class="form-control selectpicker" name="major" data-live-search="true" data-size="5">
+  					<?php print_majors(); ?>
+  				</select>
+      </div>
+      
+      <!-- Major Edit Btn or Save/Cancel -->
+      <div id="majorBtns" class="col-sm-2" style="text-align: left">
+        	<a id="majorEditBtn" href="#" class="btn btn-sm btn-warning" onclick="editMajor()">Edit</a>
+    			<button type="submit" name="majorSaveBtn" value="Submit" id="majorSaveBtn" class="btn btn-sm btn-success" style="display: none;">Save</button>
+    			<a id="majorCancelBtn" href="#" class="btn btn-sm btn-info" onclick="cancelEdit('major')" style="display: none;">Cancel</a>
+    	</div>
+  	</form>
+  	
 	</div>
 
-	<!-- Code for user's cycle -->
-	<div class="row-fluid col-sm-6 col-sm-offset-3 text-center">
-		<h4>
-			Your cycle is
-			<span>
-			<div style="display: inline-block;" id="cycleName"><?php echo "{$_SESSION['user_cycle_name']}."; ?></div>
-			<a id="cycleEditBtn" href="#" class="btn btn-xs btn-warning" onclick="editCycle()">Edit</a>
-			</span>
-		</h4>
-	</div>
-	<div class="row col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5 text-center">
-		<span id="cycleSpan" style="display: none;">
-			<form id="cycleForm" name="cycleForm" method="post" action="update.php" onsubmit="return saveCycle();">
-				<input type="hidden" name="newCycleId" id="newCycleId" value="">
-				<select id="selectCycle" class="form-control selectpicker" name="cycle"> 
-				<!-- Makes no sense but ok. -->
-					<?php if ($_SESSION['user_cycle'] == 1) { ?>
-						<option selected="selected" value="1">Fall-Winter</option>
-						<option value="2">Spring-Summer</option>
-					<?php } else { ?>
-						<option value="1">Fall-Winter</option>
-						<option selected="selected" value="2">Spring-Summer</option>
-					<?php } ?>
-				</select>
-				<div style="padding-top: 5px;"> <!-- Tmp inline style -->
-				<button type="submit" name="cycleSaveBtn" value="Submit" id="cycleSaveBtn" class="btn btn-sm btn-success" style="display: none;">Save</button>
-				<a id="cycleCancelBtn" href="#" class="btn btn-sm btn-info" onclick="cancelEdit('cycle')" style="display: none;">Cancel</a>
-				</div>
-			</form>
-		</span>
-	</div>
-
-	<!-- Code for user's program -->
-	<div class="row-fluid col-sm-6 col-sm-offset-3 text-center">
-		<h4>
-			Your program is
-			<span>
-			<div style="display: inline-block;" id="programName"><?php echo "{$_SESSION['user_program_name']}."; ?></div>
-			<a id="programEditBtn" href="#" class="btn btn-xs btn-warning" onclick="editProgram()">Edit</a>
-			</span>
-		</h4>
-	</div>
-	<div class="row col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5 text-center">
-		<span id="programSpan" style="display: none;">
-			<form id="programForm" name="programForm" method="post" action="update.php" onsubmit="return saveProgram();">
-				<input type="hidden" name="newProgramId" id="newProgramId" value="">
-				<select id="selectProgram" class="form-control selectpicker" name="cycle"> 
-				<!-- Makes no sense but ok. -->
-					<?php if ($_SESSION['user_program'] == 1) { ?>
-						<option selected="selected" value="1">1 co-op</option>
-						<option value="2">3 co-ops</option>
-					<?php } else { ?>
-						<option value="1">1 co-op</option>
-						<option selected="selected" value="2">3 co-ops</option>
-					<?php } ?>
-				</select>
-				<div style="padding-top: 5px;"> <!-- Tmp inline style -->
-				<button type="submit" name="programSaveBtn" value="Submit" id="programSaveBtn" class="btn btn-sm btn-success" style="display: none;">Save</button>
-				<a id="programCancelBtn" href="#" class="btn btn-sm btn-info" onclick="cancelEdit('program')" style="display: none;">Cancel</a>
-				</div>
-			</form>
-		</span>
+	<!-- User Cycle Field -->
+	<div class="row">
+  	<div class="col-sm-6 col-sm-offset-3 text-center">
+  		<h4>
+  			Your cycle is
+  			<span>
+  			<div style="display: inline-block;" id="cycleName"><?php echo "{$_SESSION['user_cycle_name']}."; ?></div>
+  			<a id="cycleEditBtn" href="#" class="btn btn-xs btn-warning" onclick="editCycle()">Edit</a>
+  			</span>
+  		</h4>
+  	</div>
+  	<div class="col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5 text-center">
+  		<span id="cycleSpan" style="display: none;">
+  			<form id="cycleForm" name="cycleForm" method="post" action="update.php" onsubmit="return saveCycle();">
+  				<input type="hidden" name="newCycleId" id="newCycleId" value="">
+  				<select id="selectCycle" class="form-control selectpicker" name="cycle">
+  				<!-- Makes no sense but ok. -->
+  					<?php if ($_SESSION['user_cycle'] == 1) { ?>
+  						<option selected="selected" value="1">Fall-Winter</option>
+  						<option value="2">Spring-Summer</option>
+  					<?php } else { ?>
+  						<option value="1">Fall-Winter</option>
+  						<option selected="selected" value="2">Spring-Summer</option>
+  					<?php } ?>
+  				</select>
+  				<div style="padding-top: 5px;"> <!-- Tmp inline style -->
+  				<button type="submit" name="cycleSaveBtn" value="Submit" id="cycleSaveBtn" class="btn btn-sm btn-success" style="display: none;">Save</button>
+  				<a id="cycleCancelBtn" href="#" class="btn btn-sm btn-info" onclick="cancelEdit('cycle')" style="display: none;">Cancel</a>
+  				</div>
+  			</form>
+  		</span>
+  	</div>
 	</div>
 
+	<!-- User Program Field -->
+	<div class="row">
+  	<div class="row-fluid col-sm-6 col-sm-offset-3 text-center">
+  		<h4>
+  			Your program is
+  			<span>
+  			<div style="display: inline-block;" id="programName"><?php echo "{$_SESSION['user_program_name']}."; ?></div>
+  			<a id="programEditBtn" href="#" class="btn btn-xs btn-warning" onclick="editProgram()">Edit</a>
+  			</span>
+  		</h4>
+  	</div>
+  	<div class="row col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-2 col-lg-offset-5 text-center">
+  		<span id="programSpan" style="display: none;">
+  			<form id="programForm" name="programForm" method="post" action="update.php" onsubmit="return saveProgram();">
+  				<input type="hidden" name="newProgramId" id="newProgramId" value="">
+  				<select id="selectProgram" class="form-control selectpicker" name="cycle">
+  				<!-- Makes no sense but ok. -->
+  					<?php if ($_SESSION['user_program'] == 1) { ?>
+  						<option selected="selected" value="1">1 co-op</option>
+  						<option value="2">3 co-ops</option>
+  					<?php } else { ?>
+  						<option value="1">1 co-op</option>
+  						<option selected="selected" value="2">3 co-ops</option>
+  					<?php } ?>
+  				</select>
+  				<div style="padding-top: 5px;"> <!-- Tmp inline style -->
+  				<button type="submit" name="programSaveBtn" value="Submit" id="programSaveBtn" class="btn btn-sm btn-success" style="display: none;">Save</button>
+  				<a id="programCancelBtn" href="#" class="btn btn-sm btn-info" onclick="cancelEdit('program')" style="display: none;">Cancel</a>
+  				</div>
+  			</form>
+  		</span>
+	  </div>
+  </div>
+  
 	<?php
 		// Get the latest user_matched status
 		$_SESSION['user_matched'] = mysql_get_var("SELECT matched FROM Users WHERE id = " . $_SESSION['user_id'] . "");
 
 		// If the user has a match, get the match's info and display it.
-		if ($_SESSION['user_matched'] == 1) { 
+		if ($_SESSION['user_matched'] == 1) {
 			$other_user_data = get_match_info();
 		?>
-
-			<!-- <div class="row-fluid col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 text-center"> -->
-			<div class="row-fluid col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-6 col-lg-offset-3 text-center">
-				<br><hr><p class="lead">Hey, you have a match!</p>
-
-
-				<!-- Display data for marking match as completed. 
-				Add column to Users for Match_completed, if both users are 1, set as completed in Matches table and display code. 
-				-->
-			
-
-			</div>
-
-			<div class="row-fluid col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-6 col-lg-offset-3 text-center well">
-				<p>You have matched with <strong> <?php echo $other_user_data[0]['name']; ?></strong>.</p>
-				<p>You can email them at <strong> <?php echo $other_user_data[0]['email']; ?></strong></p>
+      
+      <div class="row">
+  			<div class="row-fluid col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-6 col-lg-offset-3 text-center">
+  				<br><hr><p class="lead">Hey, you have a match!</p>
+  			</div>
+      </div>
+      
+      <div class="row">
+  			<div class="row-fluid col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-6 col-lg-offset-3 text-center well">
+  				<p>You have matched with <strong> <?php echo $other_user_data[0]['name']; ?></strong>.</p>
+  				<p>You can email them at <strong> <?php echo $other_user_data[0]['email']; ?></strong></p>
+  			</div>
 			</div>
 	
 	<?php } else { // If the user does not have a match tell them they still do not. ?>
-			<div class="row-fluid col-md-6 col-md-offset-3 text-center">
-				<br><p class="lead">You do not have a match yet, but we will keep looking!</p>
+			
+			<div class="row">
+  			<div class="row-fluid col-md-6 col-md-offset-3 text-center">
+  				<br><p class="lead">You do not have a match yet, but we will keep looking!</p>
+  			</div>
 			</div>
+			
 	<?php } ?>
 
 </div>
 
-<?php 
+<?php
 
 mysql_close($con);
 require_once(TEMPLATES_PATH . "/footer.php");
@@ -167,31 +195,32 @@ require_once(TEMPLATES_PATH . "/footer.php");
 
 	window.isMatched = "<?php echo $_SESSION['user_matched']; ?>";
 
-	window.droppedMatches = document.getElementById("droppedMatches");
+	window.droppedMatches = $("droppedMatches");
 
 	// Major Window Vars
-	window.majorSaveBtn = document.getElementById("majorSaveBtn");
-	window.majorEditBtn = document.getElementById("majorEditBtn");
-	window.majorCancelBtn = document.getElementById("majorCancelBtn");
-	window.majorSpan = document.getElementById("majorSpan");
-	window.majorDiv = document.getElementById("majorName");
-	window.selectMajor = document.getElementById("selectMajor");
+	window.majorSaveBtn = $("majorSaveBtn");
+	window.majorEditBtn = $("majorEditBtn");
+	window.majorCancelBtn = $("majorCancelBtn");
+	window.majorSpan = $("majorSpan");
+	window.majorDiv = $("majorNameText");
+	window.selectMajor = $("selectMajor");
+	window.majorBeginText = $("majorBeginText");
 
 	// Cycle Window Vars
-	window.cycleSaveBtn = document.getElementById("cycleSaveBtn");
-	window.cycleEditBtn = document.getElementById("cycleEditBtn");
-	window.cycleCancelBtn = document.getElementById("cycleCancelBtn");
-	window.cycleSpan = document.getElementById("cycleSpan");
-	window.cycleDiv = document.getElementById("cycleName");
-	window.selectCycle = document.getElementById("selectCycle");
+	window.cycleSaveBtn = $("cycleSaveBtn");
+	window.cycleEditBtn = $("cycleEditBtn");
+	window.cycleCancelBtn = $("cycleCancelBtn");
+	window.cycleSpan = $("cycleSpan");
+	window.cycleDiv = $("cycleName");
+	window.selectCycle = $("selectCycle");
 
 	// Program Window Vars
-	window.programSaveBtn = document.getElementById("programSaveBtn");
-	window.programEditBtn = document.getElementById("programEditBtn");
-	window.programCancelBtn = document.getElementById("programCancelBtn");
-	window.programSpan = document.getElementById("programSpan");
-	window.programDiv = document.getElementById("programName");
-	window.selectProgram = document.getElementById("selectProgram");	
+	window.programSaveBtn = $("programSaveBtn");
+	window.programEditBtn = $("programEditBtn");
+	window.programCancelBtn = $("programCancelBtn");
+	window.programSpan = $("programSpan");
+	window.programDiv = $("programName");
+	window.selectProgram = $("selectProgram");
 
 	window.errorClassVals = "alert alert-warning";
 	window.droppedMatches.style.display = 'none';
@@ -201,14 +230,18 @@ require_once(TEMPLATES_PATH . "/footer.php");
 		
 		if (check_if_dropped()) {
 			window.majorDiv.style.display = 'none';
+			//window.majorDiv.innerHTML = "";
 			window.majorSpan.style.display = '';
 
 			window.majorEditBtn.style.display = 'none';
 			window.majorSaveBtn.style.display = '';
 			window.majorCancelBtn.style.display = '';
+			
+			window.majorBeginText.className = 'col-sm-2 col-sm-offset-3';
+			
 		}
 
-	} 
+	}
 
 	// Replace each individ. save function is the hope, also so users can submit more than one edit.
 	var saveValues = function () {
@@ -238,7 +271,7 @@ require_once(TEMPLATES_PATH . "/footer.php");
 			window.cycleCancelBtn.style.display = '';
 		}
 
-	} 
+	}
 
 	var saveCycle = function () {
 
@@ -259,10 +292,10 @@ require_once(TEMPLATES_PATH . "/footer.php");
 
 			window.programEditBtn.style.display = 'none';
 			window.programSaveBtn.style.display = '';
-			window.programCancelBtn.style.display = '';	
+			window.programCancelBtn.style.display = '';
 		}
 
-	} 
+	}
 
 	var saveProgram = function () {
 
@@ -292,12 +325,12 @@ require_once(TEMPLATES_PATH . "/footer.php");
 
 		else if (window.isMatched == 1)
 		{
-			window.droppedMatches.textContent = "By editing your profile, your current match will be dropped. The more matches you drop, the lower you go in the queue.";	
+			window.droppedMatches.textContent = "By editing your profile, your current match will be dropped. The more matches you drop, the lower you go in the queue.";
 			window.droppedMatches.className = 'alert alert-info';
 			window.droppedMatches.style.display = '';
 			return true;
 		}
-		else 
+		else
 		{
 
 		}
