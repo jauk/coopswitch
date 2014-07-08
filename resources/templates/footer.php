c3dba6fe4a73a1479c8456e87422976dda2b94e1
Justin <me@justinmaslin.com>
2014-07-08 06:52:57 -0400

    Cleanup of css

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,16 +1,17 @@
-<br><hr>
-
-<div class="container-fluid">
-		<div class="row col-md-6 col-md-offset-3 text-center">
-			<footer>
-          <p class="small">
-          Coopswitch has no affiliation with the Drexel Steinbright Career Development Center.
-          </p>
-  		    <p class="tiny">
-          Created by <a href="http://justinmaslin.com" target="_blank">Justin Maslin</a>.
-          </p>
-			</footer>
-		</div>
+<div class="container">
+    <div class="row">
+    <hr>
+        <div class="col-sm-8 col-sm-offset-2 text-center">
+            <footer>
+                <p class="small">
+                Coopswitch has no affiliation with the Drexel Steinbright Career Development Center.
+                </p>
+                <p class="tiny">
+                Created by <a href="http://justinmaslin.com" target="_blank">Justin Maslin</a>.
+                </p>
+            </footer>
+        </div>
+    </div>
 </div>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,6 +1,8 @@
 <?php
 // if (!isset($_SESSION['login']))
 // 	$_SESSION['login'] = "";
+error_reporting(0);
+@ini_set('display_errors', 0);
 
 session_start();
 session_regenerate_id(true);
@@ -15,21 +17,6 @@ foreach (glob($_SERVER['DOCUMENT_ROOT'] . "/resources/functions/*.php") as $file
     	include $filename;
     }
     // echo $filename;
-}
-
-$title = "Coopswitch";
-$slogan = "A simple way to switch co-ops."; //Get on the right cycle! Ha.
-
-if ($_SESSION['login'] == 0) {
-  if ($debug_login) {
-    $_SESSION['login'] = 1;
-    $_SESSION['user_name'] = "Test User";
-  	$_SESSION['user_major_name'] = "Computer Science";
-  	$_SESSION['user_cycle_name'] = "Spring-Summer";
-  	$_SESSION['user_program_name'] = "3 co-ops";
-  	$_SESSION['user_matched'] = 1;
-    $_SESSION['user_email_verified'] = 1;
-  }
 }
 
 ?>
@@ -57,7 +44,7 @@ if ($_SESSION['login'] == 0) {
 	</style>
 	
 	<script src="../js/global.js"></script>
-	<script src="../js/bootstrap-select.js"></script>
+	<!-- <script src="../js/bootstrap-select.js"></script> -->
 	
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
@@ -65,61 +52,69 @@ if ($_SESSION['login'] == 0) {
 </head>
 
 <body>
-	<div class="container-fluid">
-		<div class="row col-md-6 col-md-offset-3 text-center">
-			<h1><?php echo "$title"; ?></h1>
-			<h4><?php echo "$slogan"; ?> </h4>
-		</div>
-		<div class="row col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 text-center">
-			<ul class="nav nav-pills nav-justified">
-				<li>
-					<a href="/">Home</a>
-				</li>
-				<li>
-					<a href="/about">About</a>
-				</li>
-				<li>
-					<a href="/stats">Stats</a>
-				</li>
-				<li>
-					<a href="/check">Matches</a>
-				</li>
-			</ul>
+	<div class="container">
+
+		<!-- Site Title -->
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2 text-center">
+				<h1><?php echo SITE_NAME; ?></h1>
+				<h4><?php echo SITE_SLOGAN; ?> </h4>
+			</div>
 		</div>
 
-			<!-- <div class="panel panel-default"> <br /> -->
-		<br><br>
+		<!-- Site Nav Main -->
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3 text-center">
+				<ul class="nav nav-pills nav-justified">
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="/stats">Stats</a>
+					</li>
+					<li>
+						<a href="/check">Matches</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 
-				<?php if (!isset($_SESSION['login'])) { //if ($_SESSION['login'] == "") { ?>
-  				<div class="row-fluid col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 text-center">
-  					<br><form class="form-inline" role="form" name="login_form" method="post" action="/login.php">
-  						<fieldset>
-  							<div class="form-group">
-  						    	<label class="sr-only" for="email">Email address</label>
-  						   		<input type="email" class="form-control" name="email" id="email" placeholder="Email">
-  	  						</div>
-  	  						<div class="form-group">
-  						    	<label class="sr-only" for="password">Password</label>
-  						    	<input type="password" class="form-control" name="password" id="password" placeholder="Password">
-  							</div>
-  					   		<button type="submit" class="btn btn-default btn-success">Sign In</button>
-  					    </fieldset>
-  					</form>
-  					<hr>
-  				</div>
+
+		<div class="row">
+			<div class="text-center" style="padding-top: 20px;">
+
+				<?php if (!isset($_SESSION['login'])) { ?>
+
+				<form class="form-inline" role="form" name="login_form" method="post" action="/login.php">
+					<fieldset>
+						<div class="form-group">
+					    	<label class="sr-only" for="email">Email address</label>
+					   		<input type="email" class="form-control" name="email" id="email" placeholder="Email">
+  						</div>
+  						<div class="form-group">
+					    	<label class="sr-only" for="password">Password</label>
+					    	<input type="password" class="form-control" name="password" id="password" placeholder="Password">
+						</div>
+				   		<button type="submit" class="btn btn-default btn-success">Sign In</button>
+				    </fieldset>
+				</form>
+
 				<?php } else { ?>
 
-					<div class="row-fluid col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center">
-						<br><p class="lead">
-							Hey, <?php echo $_SESSION['user_name']; ?>.&nbsp;&nbsp;&nbsp;&nbsp;
-							<a href="/account.php"><button type="button" class="btn btn-primary" >Profile</button></a>
-							<a href="/logout.php"><button type="button" class="btn btn-danger">Logout</button></a>
-						</p>
-						<hr>
-					</div>
+				<p class="lead">
+					Hey, <?php echo $_SESSION['user_name']; ?>.&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="/account.php"><button type="button" class="btn btn-primary" >Profile</button></a>
+					<a href="/logout.php"><button type="button" class="btn btn-danger">Logout</button></a>
+				</p>
 
 				<?php } ?>
-				
-		<!--	</div> -->
+
+			</div>
 		</div>
-	<!-- </div> -->
+	
+		<hr>
+
+	</div> <!-- End Header Container -->
