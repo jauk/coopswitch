8be752acba8a3bcc0e1808c428edc6dd0051e8c3
Justin <me@justinmaslin.com>
2014-07-08 06:54:22 -0400

    Cleanup

--- about.php
+++ about.php
@@ -3,27 +3,31 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/config.php");
 require_once(TEMPLATES_PATH . "/header.php");
 ?>
 
-<div class="jumbotron">
-	<div class="container-fluid text-center">
-		<div class="form-group row col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
+<div class="container">
+<div class=" col-sm-6 col-sm-offset-3">
+	<div class="row">
+		<div class="text-center text-primary">
 			<h2>What is this website?</h2>
 		</div>
 	</div>
-</div>
 
-<div class="container-fluid text-center">
-	<div class="form-group row col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">	
-		<div class="bg-info well-lg">
-			<p>Coopswitch was created to help Drexel students quickly and easily find someone to switch co-op cycles with.
-				Drexel requires you to find someone in your major and this can be tough!
-			</p>
-			<p>Coopswitch allows you to automatically find someone with no hassle! The more people that use it,
-				the more effective it will be!
-			</p>
+	<div class="row">
+		<div class="lead text-center">
+			<p>Coopswitch automates the co-op switch process to save Drexel students time.</p>
+		</div>
+	</div>
+
+	<div class="row">	
+		<div class="bg-info text-center" style="padding: 20px;">
+			<p>Coopswitch was created to help Drexel students quickly and easily find someone to switch co-op cycles with.</p>
+			<br>
+			<p>As more people register, the match frequency will be higher.</p>
 		</div>
 	</div>
 </div>
+</div>
 
+<br>
 <?php
 require_once(TEMPLATES_PATH . "/footer.php");
 ?>
