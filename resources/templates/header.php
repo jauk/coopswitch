5d8e20fcc4e77f56a47415a19aa4d93ef95819c0
Justin <me@justinmaslin.com>
2014-08-01 15:37:35 -0400

    Test

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -9,7 +9,7 @@ ob_start();
 session_start();
 session_regenerate_id(true);
 
-
+echo $_SERVER['SERVER_NAME'];
 //include('/var/www/scripts.php');
 
 // Include useful scripts so I do not have to on each page.
