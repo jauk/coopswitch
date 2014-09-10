300f0f0acafc209914ec1e0f1e5d0f3948433c07
Justin <me@justinmaslin.com>
2014-09-09 22:31:35 -0400

    Move some functions (pass validation, error setting) to the global js file

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ $formElementErrClass = "";
 	  				</div>
 	  				<div class="<?php echo $formErrorWidth ?>">
 		  				<span class="help-block error"><div id="passwordError"><p class="alert text-info bg-info">Do not use your Drexel One password.</p></div></span>
-					</div>	 	  				
+						</div>	 	  				
 	  			</div>
 
 
@@ -367,42 +367,6 @@ $formElementErrClass = "";
 		// Scroll to form on page and have it fade in/down or something.
 	}
 
-	var setError = function(fieldMainDiv, fieldErrorDiv, fieldFeedbackDiv, errorMessage) {
-
-		var mainDiv = fieldMainDiv;
-		var errorDiv = fieldErrorDiv;
-
-		if (fieldFeedbackDiv != "") {
-			var feedbackDiv = fieldFeedbackDiv;
-			feedbackDiv.className = window.feedbackError;
-		}
-
-		var error = errorMessage;
-
-		errorDiv.textContent = error;
-		errorDiv.style.display = '';
-		errorDiv.className = window.errorClassVals;
-
-		mainDiv.className = "form-group has-feedback has-error";
-
-	}
-
-	var removeError = function(fieldMainDiv, fieldErrorDiv, fieldFeedbackDiv) {
-
-		var mainDiv = fieldMainDiv;
-		var errorDiv = fieldErrorDiv;
-
-		if (fieldFeedbackDiv != "") {
-			var feedbackDiv = fieldFeedbackDiv;
-			feedbackDiv.className = window.feedbackSuccess;
-		}
-
-		mainDiv.className = "form-group has-feedback has-success";
-
-	 	errorDiv.style.display = 'none';
-
-	}
-
 	var validate_name = function () {
 
 		var name = id("user_name").value;
@@ -546,74 +510,6 @@ $formElementErrClass = "";
 	var showConfirm = function () {
 		id("user_pass_confirm").style.display = '';
 
-	}
-
-	var validate_password = function () {
-
-		var password = id("user_pass").value;
-		var password2 = id("user_pass_confirm").value;
-
-		var passwordErrorDiv = id("passwordError");
-		//var passwordErrorDiv = "";
-		var passwordDiv = id("passwordDiv");
-		var passwordFeedback = id("passwordFeedback");
-
-		if (password2 == "" && window.hasEnteredAgain) {
-
-			errors.password = 1;
-			error = "You need a password.";
-			setError(passwordDiv, passwordErrorDiv, passwordFeedback, error);	
-			console.log("This first if");
-		}
-
-		else if ((password != password2) && window.hasEnteredAgain) {
-
-			// if (password2 == "" && !window.hasEnteredAgain) {
-			// 	window.hasEnteredAgain = true;
-			// }
-
-			// else {
-				errors.password = 1;
-
-				error = "Passwords do not match.";
-				setError(passwordDiv, passwordErrorDiv, passwordFeedback, error);
-
-				console.log(error);
-	//		}
-
-		}
-
-		else {
-
-			if (password.length < 6 && password.length > 0) {
-
-			 	passwordErrorDiv.style.display = '';
-			 	passwordErrorDiv.className = 'alert text-warning bg-warning';
-				// passwordErrorDiv.className = window.errorClassVals;
-				passwordErrorDiv.textContent = "You should use a longer password.";
-
-				passwordDiv.className = "form-group has-feedback has-warning";
-				passwordFeedback.className = window.feedbackWarning;
-				console.log("Pass warning");
-			}
-
-			else {
-				removeError(passwordDiv, passwordErrorDiv, passwordFeedback);
-			}
-
-			errors.password = 0;
-		}
-
-		validate_form();
-
-	}
-
-	var passwordConfirm = function () {
-
-		window.hasEnteredAgain = true;
-		console.log("Password 2 entered.");
-
-		validate_password();
 	}
 
 	var checkmajor = function () {
