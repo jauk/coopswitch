ce21ffef20f92b90a55a41566cc7d07ca86bada9
Justin <me@justinmaslin.com>
2014-10-02 16:27:56 -0400

    Setting up new structure

--- resources/user.php
+++ resources/user.php
@@ -1,18 +1,32 @@
 <?php
 
 	session_start();
-	
-	if ($_GET["g"] == "name") {
 
-			if (!isset($_SESSION['user_name'])) {
-			//	throw new Exception('No name found. Is user logged in?');
-				echo("Error: No Name.");
-			}
-			else {
-				$name = $_SESSION['user_name'];
-				echo $name;
-			}
-	 	}
+	switch ($_SERVER['QUERY_STRING']) {
+
+		case 'g=name' :
+			$name = (isset($_SESSION['user_name'])) ? $_SESSION['user_name'] : "Error: No Name.";
+			echo $name;
+			break;
+
+		case 'g=email' :
+			$email = (isset($_SESSION['user'])) ? $_SESSION['user'] : "Error: No Email.";
+			echo $email;
+			break;
+
+	}
+
+	// if ($_GET["g"] == "name") {
+
+	// 		if (!isset($_SESSION['user_name'])) {
+	// 		//	throw new Exception('No name found. Is user logged in?');
+	// 			echo("Error: No Name.");
+	// 		}
+	// 		else {
+	// 			$name = $_SESSION['user_name'];
+	// 			echo $name;
+	// 		}
+	// }
 
 
 ?>
