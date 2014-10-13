c94d9c3ec0b148d409b7874beb62b7d01e0a9dd2
Justin <me@justinmaslin.com>
2014-10-12 21:57:43 -0400

    Working a bit more efficient and with mysqli

--- update.php
+++ update.php
@@ -2,52 +2,43 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/config.php");
 	require_once(TEMPLATES_PATH . "/header.php");
 	include(FUNCTION_PATH . "/connect.php");
+	// include(FUNCTION_PATH . "/scripts.php");
+
 
 	// Get the user we are talking to and save them into a local array.
 	// $query = "SELECT * FROM Users WHERE id = " . $_SESSION['user_id'];
 	// $result = mysql_query($query);
 	// $row = mysql_fetch_array($result);
 
+	$user_data = array();
 	$user_data = getUserDataFromId($_SESSION['user_id']);
 
-	// Need to tell which value is being updated too
-	// Update for one master form soon
-
-	if (isset($_POST['newMajorId'])) {
-		$newUserMajor = test_input($_POST['newMajorId']);
-
-		updateField("major", $newUserMajor);
+	foreach ($_POST as $key => $value) {
+			$key = test_input($key);
+			$value = test_input($value);
+		  updateField($key, $value);
 	}
 
-	if (isset($_POST['newCycleId'])) {
-		$newUserCycle = test_input($_POST['newCycleId']);
-
-		updateField("cycle", $newUserCycle);
-	}
-
-	if (isset($_POST['newProgramId'])) {
-		$newUserProgram = test_input($_POST['newProgramId']);
-
-		updateField("num_year_program", $newUserProgram);
-	}
+	check_for_match();
 
 	// ON ALL UPDATES UNDO THE MATCHES (For both users). IMP. Also verify major actually changes in db.
 
 	//echo $newUserMajor;
 
 
-	check_for_match();
-	header("Location: account.php");
 
 	function updateField($field, $newFieldVal) {
 
 		global $user_data;
+		global $con;
 
 		// Update the field.
-		if ($user_data[$field] != $newFieldVal) {
-			$query = 'UPDATE Users SET ' . $field . ' = ' . $newFieldVal . ' WHERE id = ' . $_SESSION['user_id'];
+		//if ($user_data[$field] != $newFieldVal) {
+			$sql = 'UPDATE Users SET ' . $field . ' = "' . $newFieldVal . '" WHERE id = ' . $_SESSION['user_id'];
 
-			$result = mysql_query($query);
+			if (!$con->query($sql)) {
+		  	trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $con->error, E_USER_ERROR);		
+			}
 
 			// Change session vars.
 			switch ($field):
@@ -57,20 +48,14 @@
 					break;
 				case "cycle":
 					$_SESSION['user_cycle'] = $newFieldVal;
-					if ($newFieldVal == 1)
-						$_SESSION['user_cycle_name'] = "Fall-Winter";
-					else if ($newFieldVal == 2)
-						$_SESSION['user_cycle_name'] = "Spring-Summer";
+					$_SESSION['user_cycle_name'] = getName("cycle", $newFieldVal);
 					break;
 				case "num_year_program":
 					$_SESSION['user_program'] = $newFieldVal;
-					if ($newFieldVal == 1)
-						$_SESSION['user_program_name'] = "1 co-op";
-					else if ($newFieldVal == 2)
-						$_SESSION['user_program_name'] = "3 co-ops";
+					$_SESSION['user_program_name'] = getName("program", $newFieldVal);
 					break;
 			endswitch;	
-		}
+	//	}
 	}
 
 	function check_for_match($user_data = NULL, $withdraw = NULL) {
@@ -86,29 +71,27 @@
 			// Get the ID of the other user.
 			$other_user_id = mysql_get_var("SELECT id FROM Users WHERE Matches_id = " . $user_data['Matches_id'] . " AND id != " . $user_data['id']);
 
-			$query = "SELECT * FROM Users WHERE id = " . $other_user_id;
-			$result = mysql_query($query);
-			$row = mysql_fetch_array($result);
-
-			$other_user_data = $row;
+			$sql = "SELECT * FROM Users WHERE id = " . $other_user_id;
+			$result = $con->query($sql);
+			$other_user_data = $result->fetch_array(MYSQLI_ASSOC);
 
 			// Reset the match vars for both users
-			$query = "UPDATE Users SET matched = 0 WHERE id = " . $user_data['id'] . " OR id = " . $other_user_data['id'];
-		    $result = mysql_query($query);
+			$sql = "UPDATE Users SET matched = 0 WHERE id = " . $user_data['id'] . " OR id = " . $other_user_data['id'];
+	    $result = $con->query($sql);
 
-		    $query = "UPDATE Users SET Matches_id = 0 WHERE id = " . $user_data['id'] . " OR id = " . $other_user_data['id'];
-		    $result = mysql_query($query);
+	    $sql = "UPDATE Users SET Matches_id = 0 WHERE id = " . $user_data['id'] . " OR id = " . $other_user_data['id'];
+	    $result = $con->query($sql);
 
-		    // Should we delete row from Matches or keep for historical purposes? Delete for now.
-		   	$query = "DELETE FROM Matches WHERE id = " . $user_data['Matches_id'];
-		   	$result = mysql_query($query);
-		   
-		   	// Add the dropped match to db for user dropping (add to current)
-		   	// Do not add dropped match if reason = user unresponsive (unsure of actual situation).
-		   	if ($withdraw != 2) {
-		   		$query = "UPDATE Users SET dropped_matches = " . ($user_data['dropped_matches']+1) . " WHERE id = " . $user_data['id'];
-		   		$result = mysql_query($query);
-		   	}
+	    // Should we delete row from Matches or keep for historical purposes? Delete for now.
+	   	$sql = "DELETE FROM Matches WHERE id = " . $user_data['Matches_id'];
+	    $result = $con->query($sql);
+	   
+	   	// Add the dropped match to db for user dropping (add to current)
+	   	// Do not add dropped match if reason = user unresponsive (unsure of actual situation).
+	   	if ($withdraw != 2) {
+	   		$sql = "UPDATE Users SET dropped_matches = " . ($user_data['dropped_matches']+1) . " WHERE id = " . $user_data['id'];
+	    	$result = $con->query($sql);
+	   	}
 
 
 		   	//Lets reset those session vars, too.
@@ -137,7 +120,7 @@
 
 		   		// Set as withdrawn.
 		   		$sql = 'UPDATE Users SET withdraw = 1 WHERE id = '.$other_user_data['id'];
-		   		$result = mysql_query($sql);
+		    	$result = $con->query($sql);
 
 		   		
 
@@ -151,7 +134,7 @@
 	// Use header location thing go back account page display "UPDATED" message
 	// Use a session var for now that resets after displaying message, not very efficient. Maybe use GET eventually
 	
-	mysql_close($con);
+	//mysql_close($con);
 ?>
 
 <!-- start: 150 lines -->
