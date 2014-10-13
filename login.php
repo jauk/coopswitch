41fc1e73f8b28c9a9f76f04f3cb12b181095aadb
Justin <me@justinmaslin.com>
2014-10-12 21:58:07 -0400

    Changing around how session vars are set

--- login.php
+++ login.php
@@ -11,16 +11,10 @@
 	$upass = test_input($_POST['password']);
 
   // Make sure it is a valid email address
-	if (!filter_var($umail, FITLER_VALIDATE_EMAIL)) {
-		header("Location: /error.php?msg=1");
-	}
+	// if (!filter_var($umail, FITLER_VALIDATE_EMAIL)) {
+	// 	header("Location: /error.php?msg=1");
+	// }
 
-
-	if ($db_found) {
-	}
-	else {
-		$errorMessage = "Error logging on. Database not found.";
-	}
 
 	/* Learn about quote_smart funcion for sql injection protection! IMPORTANT! */
 	// $umail = quote_smart($uname, $con);
@@ -30,28 +24,36 @@
 	$upass=sha1($upass);
 
 	$sql = "SELECT * FROM Users WHERE email = '$umail' AND password = '$upass'";
-	$result = mysql_query($sql);
+	$result = $con->query($sql);
 
 	if (!$result) {
 		die(mysql_error());
 		$errorMessage = "Error logging on. 2";
 	}
 
-	$num_rows = mysql_num_rows($result);
+	$num_rows = $result->num_rows;
 
 	if ($num_rows == 1) { // Because there should only be one result or there is a big problem.
 
     // Init blank array for user
 		$user_data = array();
 
-    // Grab the row with user data
-	  $row = mysql_fetch_array($result);
-	  $user_data = $row; // Save the user's info in an array.
+    // Grab the row with user data and save into array.
+	  $user_data = $result->fetch_assoc();
+
+	  //$user_data = $row; // Save the user's info in an array.
 
 		$errorMessage = "Logged on!"; // Hooray, we are logged on.
 
 		/* Get information from user's row and save in session variables */
 		login_user($user_data);
+
+		// Var names
+		// TODO: foreach for all session vars
+    $_SESSION['user_major_name'] = getMajorName($user_data['major']);
+    $_SESSION['user_cycle_name'] = getName("cycle", $user_data['cycle']);
+    $_SESSION['user_program_name'] = getName("program", $user_data['num_year_program']);
+
 
     // Redirect to accounts page
 		header("Location: /account.php");
