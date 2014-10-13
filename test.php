758091129ce262d639898fda69b5582f76d4ea69
Justin <me@justinmaslin.com>
2014-10-12 21:59:32 -0400

    Test

--- test.php
+++ test.php
@@ -1,1 +1,9 @@
- 
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/config.php");
+require_once(TEMPLATES_PATH . "/header.php");
+include_once(FUNCTION_PATH . "/connect.php");
+
+print_majors();
+
+?> 
